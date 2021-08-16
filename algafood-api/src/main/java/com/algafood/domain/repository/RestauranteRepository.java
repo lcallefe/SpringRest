283b11c74b729cf6616baf6e6c98522031f18bbe
lcallefe <luciana_donadio@hotmail.com>
2021-08-16 04:12:52 -0300

    resolvendo problema do n+1 restaurante x cozinha

--- algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
@@ -1,11 +1,15 @@
 package com.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algafood.domain.model.Restaurante;
 
 @Repository
 public interface RestauranteRepository extends CustomizedJpaRepository<Restaurante, Long>, CustomizedRestauranteRepository, JpaSpecificationExecutor<Restaurante> {
-
+	@Query("from Restaurante r join r.cozinha left join fetch formasPagamento")
+	List<Restaurante> findAll();
 }
